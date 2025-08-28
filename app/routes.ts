32a3055d33b31c6b6061be282fdb8b285e030113
jhunpaul-prog <127946472+jhunpaul-prog@users.noreply.github.com>
2025-08-28 22:53:11 +0800

    Update routes.ts / stats

--- app/routes.ts
+++ app/routes.ts
@@ -111,7 +111,7 @@ export default [
     file: "pages/ResidentDoctor/Settings/AccountSettings.tsx",
   },
   { path: "/saved-list", file: "pages/ResidentDoctor/Settings/SavedList.tsx" },
-  { path: "/my-stats", file: "pages/ResidentDoctor/Settings/Stats.tsx" },
+  { path: "/my-stats", file: "pages/ResidentDoctor/Settings/stats.tsx" },
   {
     path: "/My-Papers",
     file: "pages/ResidentDoctor/Settings/UserResearch.tsx",
